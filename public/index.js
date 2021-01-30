69f22438ff9a6e85ef0d1329c2182e3427f47464
teakyung25 <taekyungkim25@gmail.com>
2021-01-29 19:28:14 -0700

    Started on landing page

---
+++ public/index.js
@@ -0,0 +1,15 @@
+"use strict";
+function ready() {
+    alert("This is the javascript. I'm working!");
+    console.log('hello world');
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
+
+/* 
+this is a multiline comment. we should be able to chat here
+THE MITOCHONDRIA IS THE POWER HOUSE OF THE CELL... words of wisdom
+thank you.
+You can thank me when your graduating from harvord
+*/
